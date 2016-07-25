697bf228ee4f2b660bc64c0489b32a0ac132e77e
Nisheeth Barthwal <nbaztec@gmail.com>
2016-07-25 23:36:13 +0530

    added CORS module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes');
 const app = express();
@@ -11,6 +12,7 @@ config.init(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', routes);
 
